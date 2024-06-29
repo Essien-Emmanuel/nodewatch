a0b584c09739f1ba29861ebed0b50a300b253215
essien emmanuel <essienemma300@gmail.com>
2024-06-29 08:37:49 +0100

    worked on starting a second program from our code to enable watchit run any specific file

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const { spawn } = require('child_process');
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
 
+
+const watchFile = (filename) => {
+  const start = debounce(() => {
+    spawn('node', [filename], { stdio: 'inherit' })
+  }, 100)
+  
+  chokidar.watch(process.cwd())
+  .on('add', start)
+    .on('change', start)
+    .on('unlink', start);
+}
+
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of the file to execute!')
-  .action((args) => {
-    console.log(args);
+  .action( async (args) => {
+    const { filename } = args;
+    const $filename = filename || 'index.js';
+
+    try {
+      await fs.promises.access($filename)
+    } catch (error) {
+      throw new Error(`Could not find the file ${$filename}`);
+    }
+
+    watchFile($filename)
   });
 
 program.parse(process.argv)
-
-// const start = debounce(() => {
-//   console.log('STARTING USERS PROGRAM');
-// }, 100)
-
-// chokidar.watch(process.cwd())
-//   .on('add', start)
-//   .on('change', () => console.log('FILE CHANGED'))
-//   .on('unlink', () => console.log('FILE UNLINKED'))
