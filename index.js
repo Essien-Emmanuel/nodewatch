f58422b25f15448586a929249eff18b659092f7a
essien emmanuel <essienemma300@gmail.com>
2024-06-29 07:55:48 +0100

    handling cli tool with caporal

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
 
-const start = debounce(() => {
-  console.log('STARTING USERS PROGRAM');
-}, 100)
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of the file to execute!')
+  .action((args) => {
+    console.log(args);
+  });
 
-chokidar.watch(process.cwd())
-  .on('add', start)
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'))
+program.parse(process.argv)
+
+// const start = debounce(() => {
+//   console.log('STARTING USERS PROGRAM');
+// }, 100)
+
+// chokidar.watch(process.cwd())
+//   .on('add', start)
+//   .on('change', () => console.log('FILE CHANGED'))
+//   .on('unlink', () => console.log('FILE UNLINKED'))
